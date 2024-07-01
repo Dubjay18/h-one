866380cd82a6d158c8ccfc2c0af37d87837210b5
Dubjay18 <jejeniyi7@gmail.com>
2024-07-01 12:19:46 +0100

    fix: ip

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -9,7 +9,6 @@ import (
 func (s *Server) DisplayDetailsHandler(c *gin.Context) {
 	visitorName := c.Query("visitor_name")
 	ip := GetClientIP(c)
-	ip = "8.8.8.8"
 	location, err := GetLocationFromIP(ip)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
